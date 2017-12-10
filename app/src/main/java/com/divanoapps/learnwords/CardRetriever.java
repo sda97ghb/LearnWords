eea8dca3b4b4c938d7020c4adc241d6eae08b81c
Dmitry Smirnov <sda97g@gmail.com>
2017-12-10 21:19:26 +0500

    Added exercises.

--- app/src/main/java/com/divanoapps/learnwords/CardRetriever.java
+++ app/src/main/java/com/divanoapps/learnwords/CardRetriever.java
@@ -1,5 +1,14 @@
 package com.divanoapps.learnwords;
 
+import com.divanoapps.learnwords.entities.Card;
+import com.divanoapps.learnwords.entities.Deck;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Created by dmitry on 09.10.17.
  */
@@ -11,5 +20,83 @@ public class CardRetriever {
         random,
         higher30,
         lower30
+    }
+
+    private List<Card> mCards;
+
+    private RetrieveOrder mRetrieveOrder;
+
+    public CardRetriever(Order order, Deck deck) {
+        mCards = deck.getCardsAsList();
+
+        switch (order) {
+            case alphabetical: mRetrieveOrder = new AlphabeticalRetrieveOrder(); break;
+            case file:         mRetrieveOrder = new FileRetrieveOrder();         break;
+            case random:       mRetrieveOrder = new RandomRetrieveOrder();       break;
+            
+            default:           mRetrieveOrder = new FileRetrieveOrder();         break;
+        }
+    }
+
+    public Card getNext() {
+        return mRetrieveOrder.getNext();
+    }
+
+    private interface RetrieveOrder {
+        Card getNext();
+    }
+
+    private class AlphabeticalRetrieveOrder implements RetrieveOrder {
+        List<Card> mSortedCards;
+        private int mCurrentCardIndex = -1;
+
+        @Override
+        public Card getNext() {
+            if (mSortedCards == null) {
+                mSortedCards = new LinkedList<>(mCards);
+                Collections.sort(mSortedCards, (o1, o2) -> o1.getWord().compareTo(o2.getWord()));
+            }
+
+            ++ mCurrentCardIndex;
+            if (mCurrentCardIndex >= mSortedCards.size())
+                return null;
+            else
+                return mSortedCards.get(mCurrentCardIndex);
+        }
+    }
+
+    private class FileRetrieveOrder implements RetrieveOrder {
+        private int mCurrentCardIndex = -1;
+
+        @Override
+        public Card getNext() {
+            ++ mCurrentCardIndex;
+            if (mCurrentCardIndex >= mCards.size())
+                return null;
+            else
+                return mCards.get(mCurrentCardIndex);
+        }
+    }
+
+    private class RandomRetrieveOrder implements RetrieveOrder {
+        @Override
+        public Card getNext() {
+            if (mCards.isEmpty())
+                return null;
+
+            List<Integer> sums = new ArrayList<>(mCards.size() + 1);
+            sums.set(0, 0);
+            for (int i = 1; i < mCards.size(); ++ i)
+                sums.set(i, sums.get(i - 1) + mCards.get(i).getDifficulty());
+
+            int sum = sums.get(sums.size() - 1);
+            int randN = ThreadLocalRandom.current().nextInt(0, sum);
+
+            int index = 0;
+            while (sums.get(index) < randN)
+                ++ index;
+
+            return mCards.get(index);
+        }
     }
 }
---
+++ app/src/main/java/com/divanoapps/learnwords/activities/ExerciseActivity.java
@@ -0,0 +1,241 @@
+package com.divanoapps.learnwords.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.divanoapps.learnwords.CardRetriever;
+import com.divanoapps.learnwords.R;
+import com.divanoapps.learnwords.data.DB;
+import com.divanoapps.learnwords.data.RequestError;
+import com.divanoapps.learnwords.dialogs.MessageOkDialogFragment;
+import com.divanoapps.learnwords.entities.Card;
+import com.divanoapps.learnwords.entities.Deck;
+import com.divanoapps.learnwords.entities.DeckId;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ExerciseActivity extends AppCompatActivity {
+
+    private enum Side {
+        First,
+        Second
+    }
+
+    TextView mWordView;
+    TextView mWordCommentView;
+    TextView mTranslationView;
+    TextView mTranslationCommentView;
+
+    View mCardPartsSeparator;
+
+    Button mEasyButton;
+    Button mHardButton;
+    Button mHideButton;
+
+    View mRootView;
+
+    CardRetriever.Order mOrder;
+    Deck mDeck;
+    CardRetriever mCardRetriever;
+    Card mCurrentCard;
+    Side mCurrentSide;
+
+    public static String getDeckIdExtraName() {
+        return "DECK_ID_EXTRA";
+    }
+
+    public static String getOrderExtraName() {
+        return "ORDER_EXTRA";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_excercise);
+
+        findUi();
+        setOnClickListeners();
+
+        Intent intent = getIntent();
+        if (!intent.hasExtra(getDeckIdExtraName()) ||
+            !intent.hasExtra(getOrderExtraName()))
+            finish();
+        mOrder = (CardRetriever.Order) intent.getSerializableExtra(getOrderExtraName());
+        DB.getDeck((DeckId) intent.getSerializableExtra(getDeckIdExtraName()))
+            .setOnDoneListener(this::startExerciseForDeck)
+            .setOnErrorListener(this::finish)
+            .execute();
+    }
+
+    private void startExerciseForDeck(Deck deck) {
+        mDeck = deck;
+        mCardRetriever = new CardRetriever(mOrder, mDeck);
+        showNextCard();
+    }
+
+    private void showNextCard() {
+        hideSecondSide();
+        mCurrentSide = Side.First;
+
+        mCurrentCard = mCardRetriever.getNext();
+        if (mCurrentCard == null)
+            finishExercise();
+        else
+            setTextFromCurrentCard();
+    }
+
+    private void setTextFromCurrentCard() {
+        mWordView.setText(mCurrentCard.getWord());
+        mWordCommentView.setText(mCurrentCard.getWordComment());
+        mTranslationView.setText(mCurrentCard.getTranslation());
+        mTranslationCommentView.setText(mCurrentCard.getTranslationComment());
+    }
+
+    private void hideSecondSide() {
+        mCardPartsSeparator.setVisibility(View.GONE);
+        mTranslationView.setVisibility(View.GONE);
+        mTranslationCommentView.setVisibility(View.GONE);
+    }
+
+    private void showSecondSide() {
+        mCardPartsSeparator.setVisibility(View.VISIBLE);
+        mTranslationView.setVisibility(View.VISIBLE);
+        mTranslationCommentView.setVisibility(View.VISIBLE);
+    }
+
+    private void findUi() {
+        mWordView               = (TextView) findViewById(R.id.word_view);
+        mWordCommentView        = (TextView) findViewById(R.id.word_comment_view);
+        mTranslationView        = (TextView) findViewById(R.id.translation_view);
+        mTranslationCommentView = (TextView) findViewById(R.id.translation_comment_view);
+
+        mCardPartsSeparator     =            findViewById(R.id.card_parts_separator);
+
+        mEasyButton             = (Button)   findViewById(R.id.easy_button);
+        mHardButton             = (Button)   findViewById(R.id.hard_button);
+        mHideButton             = (Button)   findViewById(R.id.hide_button);
+
+        mRootView               =            findViewById(R.id.root_layout);
+    }
+
+    private void setOnClickListeners() {
+        mEasyButton.setOnClickListener(v -> onEasyClicked());
+        mHardButton.setOnClickListener(v -> onHardClicked());
+        mHideButton.setOnClickListener(v -> onHideClicked());
+
+        mRootView.setOnClickListener(v -> onNextClicked());
+        mCardPartsSeparator.setOnClickListener(v -> onNextClicked());
+        mWordView.setOnClickListener(v -> onNextClicked());
+        mWordCommentView.setOnClickListener(v -> onNextClicked());
+        mTranslationView.setOnClickListener(v -> onNextClicked());
+        mTranslationCommentView.setOnClickListener(v -> onNextClicked());
+    }
+
+    private void onEasyClicked() {
+        if (mCurrentCard == null)
+            return;
+
+        switch (mCurrentSide) {
+            case First:  onNextClicked();      break;
+            case Second: decreaseDifficulty(); break;
+        }
+    }
+
+    private void onHardClicked() {
+        if (mCurrentCard == null)
+            return;
+
+        switch (mCurrentSide) {
+            case First:  onNextClicked();      break;
+            case Second: increaseDifficulty(); break;
+        }
+    }
+
+    private void onHideClicked() {
+        if (mCurrentCard == null)
+            return;
+
+        Card newCard = new Card.Builder(mCurrentCard)
+                .setHidden(!mCurrentCard.isHidden())
+                .build();
+        DB.saveCard(newCard)
+                .setOnDoneListener(this::showNextCard)
+                .setOnErrorListener(this::showErrorMessage)
+                .execute();
+    }
+
+    private void onNextClicked() {
+        if (mCurrentCard == null)
+            return;
+
+        switch (mCurrentSide) {
+            case First:
+                showSecondSide();
+                mCurrentSide = Side.Second;
+                break;
+            case Second:
+                showNextCard();
+                break;
+        }
+    }
+
+    private void finishExercise() {
+        mWordView.setText("Finished.");
+        mWordCommentView.setText("");
+        hideSecondSide();
+    }
+
+    private void increaseDifficulty() {
+        int difficulty = mCurrentCard.getDifficulty() + 1;
+        if (difficulty > Card.getMaxDifficulty())
+            difficulty = Card.getMinDifficulty();
+        Card newCard = new Card.Builder(mCurrentCard)
+                .setDifficulty(difficulty)
+                .build();
+        DB.saveCard(newCard)
+                .setOnDoneListener(this::showNextCard)
+                .setOnErrorListener(this::showErrorMessage)
+                .execute();
+    }
+
+    private void decreaseDifficulty() {
+        int difficulty = mCurrentCard.getDifficulty() - 1;
+        if (difficulty < Card.getMinDifficulty())
+            difficulty = Card.getMinDifficulty();
+        Card newCard = new Card.Builder(mCurrentCard)
+                .setDifficulty(difficulty)
+                .build();
+        DB.saveCard(newCard)
+                .setOnDoneListener(this::showNextCard)
+                .setOnErrorListener(this::showErrorMessage)
+                .execute();
+    }
+
+    private void showErrorMessage(String message) {
+        MessageOkDialogFragment.show(this, message);
+    }
+
+    private void showErrorMessage(RequestError error) {
+        showErrorMessage(error.getMessage());
+    }
+}
