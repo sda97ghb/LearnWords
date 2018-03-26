719dca0d0f91a34a591e019c545c37a3df36d634
sda97ghb <sda97g@gmail.com>
2018-03-26 19:25:56 +0500

    Added API entity classes

---
+++ app/src/main/java/com/divanoapps/learnwords/data/api2/ApiCardReference.java
@@ -0,0 +1,35 @@
+package com.divanoapps.learnwords.data.api2;
+
+import com.google.gson.annotations.SerializedName;
+
+public class ApiCardReference {
+    @SerializedName("word")
+    private String word;
+
+    @SerializedName("comment")
+    private String comment;
+
+    public ApiCardReference() {
+    }
+
+    public ApiCardReference(String word, String comment) {
+        this.word = word;
+        this.comment = comment;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
